eab555ac73a52016db66306a4c09bbb7849ce010
Abis <arjunjbiswas@gmail.com>
2023-02-02 15:30:20 -0500

    physics stuff

--- StateOfMatter/Assets/Scripts/PlayerController.cs
+++ StateOfMatter/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 1.0f;
+    public float jumpHeight = 2.0f;
     public float sensitivity = 10.0f;
 
     private Vector3 mouseVel, mousePosCur, mousePosPrev;
@@ -25,23 +26,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 velocity = Vector3.zero;
-
-        // -- BASIC MOVEMENT HANDLING -- \\
-        if (Input.GetKey(KeyCode.W)) velocity += camTransform.forward;
-        if (Input.GetKey(KeyCode.S)) velocity -= camTransform.forward;
-        if (Input.GetKey(KeyCode.A)) velocity -= camTransform.right;
-        if (Input.GetKey(KeyCode.D)) velocity += camTransform.right;
-        velocity.Normalize(); // Fix the diagonal speed thing
+        Vector3 movementForce = Vector3.zero;
 
         // -- CAMERA CONTROL -- \\
-
         transform.Rotate(transform.up, Input.GetAxisRaw("Mouse X") * sensitivity, Space.World);
         camTransform.Rotate(camTransform.right, -Input.GetAxisRaw("Mouse Y") * sensitivity, Space.World);
 
+        // -- BASIC MOVEMENT HANDLING -- \\
+        if (Input.GetKey(KeyCode.W)) movementForce += transform.forward * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S)) movementForce -= transform.forward * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) movementForce -= transform.right * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D)) movementForce += transform.right * Time.deltaTime;
+        movementForce.Normalize(); // Fix the diagonal speed thing
+
+        // -- JUMP -- \\
+        if (Input.GetKeyDown(KeyCode.Space)) movementForce += transform.up * jumpHeight;
+
+        // if not moving around, slow the player down
+        Vector3 movementXZ = new Vector3(movementForce.x, movementForce.y);
+        if (movementXZ.sqrMagnitude == 0)
+        {
+            Vector3 stop = -(GetComponent<Rigidbody>().velocity * 5);
+            stop.y = 0;
+            GetComponent<Rigidbody>().AddForce(stop);
+        }
 
         // Output resulting velocity
-        GetComponent<Rigidbody>().AddForce(velocity * walkSpeed * Time.deltaTime);
+        GetComponent<Rigidbody>().AddForce(walkSpeed * movementForce);
     }
 
 
