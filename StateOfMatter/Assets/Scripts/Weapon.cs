6a5511e02f46f4490606092125a6772870f4aa87
Abis <26350896+TheOneAbis@users.noreply.github.com>
2023-02-06 13:21:58 -0500

    Branch aj (#1)
    * implemented grounded / ungrounded check
    
    * stuff
    
    * implemented a max incline for jumping
    
    * some more fine tuning
    
    * more screwing around, moving on ramps is good
    
    * clamped look pitch so player can't look upside down
    
    * added crouching and airborne drag when jumping
    
    * Revert "Merge branch 'main' into branch_AJ"
    
    This reverts commit fb19fad06d07778a5fabe94d53b947fc19be2a37, reversing
    changes made to a1a2d702010b9b7fb779d1e10c128f431c0ec3df.

--- StateOfMatter/Assets/Scripts/Weapon.cs
+++ StateOfMatter/Assets/Scripts/Weapon.cs
@@ -2,60 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-public enum Modes //Will be used later to differentiate firing modes
+public enum Modes
 {
     Water,
-    Steam,
+    Fire,
     Ice
 }
 
 
 public class Weapon : MonoBehaviour
 {
-    //Handles the particles that come out when firing.
-    [SerializeField]
-    private ParticleSystem FiringSystem;
-    [SerializeField]
-    private WeaponAttackRadius AttackRadius;
-
-    [Space]
-
-    [SerializeField]
-    private int dps = 3;
-    private float effectDur = 3f;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Debug.Log("Firing");
-            Fire();
-        }
-        else
-        {
-            StopFiring();
-        }
-    }
-
-
-    //Sets particle system and hitbox to turn on and off respectively 
-    private void Fire()
-    {
-        FiringSystem.gameObject.SetActive(true);
-        AttackRadius.gameObject.SetActive(true);
-    }
-
-    private void StopFiring()
-    {
-        FiringSystem.gameObject.SetActive(false);
-        AttackRadius.gameObject.SetActive(false);
+        
     }
 }
