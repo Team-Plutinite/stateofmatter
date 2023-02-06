04e73f3b706aed42171610c945b99b8f80cff094
Abis <26350896+TheOneAbis@users.noreply.github.com>
2023-02-06 13:22:51 -0500

    Revert "Branch aj (#1)"
    This reverts commit 6a5511e02f46f4490606092125a6772870f4aa87.

--- StateOfMatter/Assets/Scripts/Weapon.cs
+++ StateOfMatter/Assets/Scripts/Weapon.cs
@@ -2,25 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Modes
+
+
+
+public enum Modes //Will be used later to differentiate firing modes
 {
     Water,
-    Fire,
+    Steam,
     Ice
 }
 
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Handles the particles that come out when firing.
+    [SerializeField]
+    private ParticleSystem FiringSystem;
+    [SerializeField]
+    private WeaponAttackRadius AttackRadius;
+
+    [Space]
+
+    [SerializeField]
+    private int dps = 3;
+    private float effectDur = 3f;
+
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Firing");
+            Fire();
+        }
+        else
+        {
+            StopFiring();
+        }
+    }
+
+
+    //Sets particle system and hitbox to turn on and off respectively 
+    private void Fire()
+    {
+        FiringSystem.gameObject.SetActive(true);
+        AttackRadius.gameObject.SetActive(true);
+    }
+
+    private void StopFiring()
+    {
+        FiringSystem.gameObject.SetActive(false);
+        AttackRadius.gameObject.SetActive(false);
     }
 }
