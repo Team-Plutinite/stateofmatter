c1df30ab69e3d501f318526bd8d1cdd56fc8f87d
utlk <tlynch247@gmail.com>
2023-03-08 16:37:54 -0500

    Fixed Issues cause by the first pressure plate merge

--- StateOfMatter/Assets/Scripts/Environment Script/PressurePlate.cs
+++ StateOfMatter/Assets/Scripts/Environment Script/PressurePlate.cs
@@ -2,47 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< Updated upstream
 
-[RequireComponent(typeof(Collider))]
-[DisallowMultipleComponent]
-public class PressurePlate : MonoBehaviour
-{
-    [SerializeField]
-    BoxCollider pressureCollider;
 
-    public delegate void PressurePlateEntered(GameObject gO);
-    public delegate void PressurePlateExited(GameObject gO);
-
-    public PressurePlateEntered OnStay;
-    public PressurePlateExited OnExit; 
-
-    //Holds the current pressure plate value.
-    bool depressed = false;
-
-    private List<GameObject> ObjectOnPlate = new List<GameObject>();   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.TryGetComponent<GameObject>(out GameObject o))
-        {
-            ObjectOnPlate.Add(o);
-            OnStay?.Invoke(o);
-            depressed = true;
-            Debug.Log(depressed);
-        }
-=======
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField]
@@ -53,29 +14,13 @@ public class PressurePlate : MonoBehaviour
     {
         pressed = true;
         Debug.Log(pressed + " On Plate");
->>>>>>> Stashed changes
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-<<<<<<< Updated upstream
-        if (other.TryGetComponent<GameObject>(out GameObject o))
-        {
-            ObjectOnPlate.Remove(o);
-            OnStay?.Invoke(o);
-            depressed = false;
-            Debug.Log(depressed);
-        }
-    }
-
-    public bool Depressed
-    {
-        set { depressed = value; }
-        get { return depressed; }
-=======
         pressed = false;
         Debug.Log(pressed + " Off Plate");
-
->>>>>>> Stashed changes
     }
+
 }
