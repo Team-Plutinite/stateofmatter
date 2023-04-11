ca133a730f7a7ec29ffb3ff5e2bda5d93840f29b
utlk <tlynch247@gmail.com>
2023-04-11 14:50:43 -0400

    Preliminary barrel work

--- StateOfMatter/Assets/Scripts/Weapon Scripts/GasAttacker.cs
+++ StateOfMatter/Assets/Scripts/Weapon Scripts/GasAttacker.cs
@@ -9,9 +9,15 @@ public class GasAttacker : MonoBehaviour
 {
     public delegate void MeltableEnteredEvent(Meltable melting);
     public delegate void MeltableExitedEvent(Meltable melting);
-
+    
     public MeltableEnteredEvent MeltEnter;
     public MeltableExitedEvent MeltExit;
+
+    public delegate void BarrelEnteredEvent(Barrel barrel);
+    public delegate void BarrelEnteredExit(Barrel barrel);
+
+    public BarrelEnteredEvent BarrelEnter;
+    public BarrelEnteredExit BarrelExit;
 
     public delegate void EnemyEnteredEvent(EnemyStats enemy);
     public delegate void EnemyExitedEvent(EnemyStats enemy);
@@ -21,6 +27,7 @@ public class GasAttacker : MonoBehaviour
 
     private List<EnemyStats> EnemiesInRadius = new List<EnemyStats>();
     private List<Meltable> MeltablesInRadius = new List<Meltable>();
+    private List<Barrel> BarrelsInRadius = new List<Barrel>();
 
     private float lifetime;
 
@@ -54,6 +61,12 @@ public class GasAttacker : MonoBehaviour
             MeltablesInRadius.Add(ice);
             MeltEnter?.Invoke(ice);
         }
+
+        if (other.TryGetComponent(out Barrel barrel))
+        {
+            BarrelsInRadius.Add(barrel);
+            BarrelEnter?.Invoke(barrel);
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -68,6 +81,12 @@ public class GasAttacker : MonoBehaviour
         {
             MeltablesInRadius.Remove(ice);
             MeltExit?.Invoke(ice);
+        }
+
+        if (other.TryGetComponent(out Barrel barrel))
+        {
+            BarrelsInRadius.Remove(barrel);
+            BarrelEnter?.Invoke(barrel);
         }
     }
 }
