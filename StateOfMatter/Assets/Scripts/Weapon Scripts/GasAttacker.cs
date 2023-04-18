ac3110d19da52d9affdb809845035d878980737f
utlk <tlynch247@gmail.com>
2023-04-18 13:28:54 -0400

    Security Feed now rolls over, Changed all meltables in the level to a prefab

--- StateOfMatter/Assets/Scripts/Weapon Scripts/GasAttacker.cs
+++ StateOfMatter/Assets/Scripts/Weapon Scripts/GasAttacker.cs
@@ -19,24 +19,38 @@ public class GasAttacker : MonoBehaviour
     public BarrelEnteredEvent BarrelEnter;
     public BarrelEnteredExit BarrelExit;
 
-    public delegate void EnemyEnteredEvent(EnemyStats enemy);
-    public delegate void EnemyExitedEvent(EnemyStats enemy);
+    public delegate void EnemyEnteredEvent(EnemyStats enemy, GameObject cloud);
+    public delegate void EnemyExitedEvent(EnemyStats enemy, GameObject cloud);
+
 
     public EnemyEnteredEvent OnStay;
     public EnemyEnteredEvent OnExit;
 
-    private List<EnemyStats> EnemiesInRadius = new List<EnemyStats>();
+    private List<EnemyStats> EnemiesHit = new List<EnemyStats>();
     private List<Meltable> MeltablesInRadius = new List<Meltable>();
     private List<Barrel> BarrelsInRadius = new List<Barrel>();
 
+
+
     private float lifetime;
+    private float lifetimeMax;
+
+    // Should the cloud knock enemies back upon contact?
+    private bool knockbackable;
 
     private void Update()
     {
         // Set inactive when lifetime runs out
         lifetime -= Time.deltaTime;
+        // cloud knocks enemies back for the first 20% of its life
+        knockbackable = lifetime >= lifetimeMax * 0.8f;
+
+        // Clear enemies hit and deactivate cloud when it's done
         if (lifetime <= 0.0f && gameObject.activeSelf)
+        {
             gameObject.SetActive(false);
+            EnemiesHit.Clear();
+        }
     }
 
     // Spawns this gas cloud at the given location with the given rotation
@@ -49,12 +63,20 @@ public class GasAttacker : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-       //This function will handle collison with various objects.
-       if (other.TryGetComponent(out EnemyStats enemy))
-       {
-            EnemiesInRadius.Add(enemy);
-            OnStay?.Invoke(enemy);
-       }
+        //This function will handle collison with various objects.
+        //This function will handle collison with various objects.
+        if (other.TryGetComponent(out EnemyStats enemy))
+        {
+            // If this is the first time enemy has been hit by this cloud
+            // and the cloud was recently created, knock the enemy back
+            if (!EnemiesHit.Contains(enemy) && knockbackable)
+            {
+                EnemiesHit.Add(enemy);
+                enemy.gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * 50f);
+                enemy.TakeDamage(5.0f);
+            }
+            OnStay?.Invoke(enemy, this.gameObject);
+        }
 
         if (other.TryGetComponent(out Meltable ice))
         {
@@ -75,10 +97,10 @@ public class GasAttacker : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.TryGetComponent(out EnemyStats enemy))
+
+        if (other.TryGetComponent(out EnemyStats enemy))
         {
-            EnemiesInRadius.Remove(enemy);
-            OnExit?.Invoke(enemy);
+            OnExit?.Invoke(enemy, this.gameObject);
         }
 
         if (other.TryGetComponent(out Meltable ice))
